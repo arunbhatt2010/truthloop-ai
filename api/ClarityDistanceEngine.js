e7064719a11ed6ff9e845524568d1c63ceff9702
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-07-05 02:27:27 +0530

    Refactor analyzeClarityDistance to use static method

--- api/ClarityDistanceEngine.js
+++ api/ClarityDistanceEngine.js
@@ -2351,8 +2351,7 @@ return label
 
 
 export function analyzeClarityDistance(data) {
-  const engine = new ClarityDistanceEngine();
-  return engine.buildContext(data);
+  return ClarityDistanceEngine.build(data);
 }
 
 export default ClarityDistanceEngine;
