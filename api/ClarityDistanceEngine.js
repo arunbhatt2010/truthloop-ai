39a71d25b7f09f8e610bbd1a417bcd11ed808a1c
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-07-05 02:21:50 +0530

    Instantiate ClarityDistanceEngine in analyzeClarityDistance

--- api/ClarityDistanceEngine.js
+++ api/ClarityDistanceEngine.js
@@ -2351,7 +2351,8 @@ return label
 
 
 export function analyzeClarityDistance(data) {
-  return ClarityDistanceEngine.buildContext(data);
+  const engine = new ClarityDistanceEngine();
+  return engine.buildContext(data);
 }
 
 export default ClarityDistanceEngine;
