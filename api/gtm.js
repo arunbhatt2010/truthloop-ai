ba119c16f64f091ef1cf2b3d35ab803aebe9941b
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-07-14 15:46:25 +0530

    Add logging for social and scope report details
    Added console logs to display types and values of socialReport and scopeReport.

--- api/gtm.js
+++ api/gtm.js
@@ -791,6 +791,13 @@ async function loadGTMIntelligenceBrain(
     };
 
     try {
+      console.log("SOCIAL TYPE:", typeof socialReport);
+console.log("SOCIAL KEYS:", Object.keys(socialReport || {}));
+console.log("SOCIAL VALUE:", JSON.stringify(socialReport, null, 2));
+
+console.log("SCOPE TYPE:", typeof scopeReport);
+console.log("SCOPE KEYS:", Object.keys(scopeReport || {}));
+console.log("SCOPE VALUE:", JSON.stringify(scopeReport, null, 2));
 console.log("SOCIAL REPORT:", JSON.stringify(socialReport, null, 2));
 console.log("SCOPE REPORT:", JSON.stringify(scopeReport, null, 2));
         const {
