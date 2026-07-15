c3a67e2d8940771d534273e6036c0751a9a8315d
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-07-15 14:07:53 +0530

    Rename function for clarity

--- api/ProfileSystemBrain.js
+++ api/ProfileSystemBrain.js
@@ -1621,7 +1621,7 @@ function detectPositionAlignment({
 
 }
 /* ============================================================
-   BUILD POSITION PROFILE
+   DETECT POSITION EVIDENCE
 ============================================================ */
 
 function detectPositionEvidence({
