70e71ae172328ea56d62bad9e36a5583c780ee6f
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-07-28 01:33:14 +0530

    Add POST method check in digitalFootprint handler

--- api/digitalFootprint.js
+++ api/digitalFootprint.js
@@ -2,19 +2,18 @@ import { loadDigitalFootprintBrain }
 from "./DigitalFootprintBrain.js";
 
 export default async function handler(req,res){
-
+if (req.method !== "POST") {
+    return res.status(405).json({
+        success: false,
+        message: "POST request required."
+    });
+}
     const {
-
-        identityPackage,
-
-        currentLoop,
-
-        currentCategory,
-
-        messages
-
-    } = req.body;
-
+    identityPackage,
+    currentLoop,
+    currentCategory,
+    messages
+} = req.body || {};
     const publicEvidencePackage =
         await loadDigitalFootprintBrain({
 
