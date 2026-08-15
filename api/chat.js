4e1161f0faf2b5bf4f3e0c7ca423a6bcace985f0
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-08-15 23:42:12 +0530

    Import loadCrossEvidenceBrain in chat.js

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,9 @@ import { runMasterBrain }
 from "./masterBrain.js";
 import { loadDigitalFootprintBrain } from "./DigitalFootprintBrain.js";
 export default async function handler(req, res) {
-
+import {
+    loadCrossEvidenceBrain
+} from "./CrossEvidenceBrain.js";
   /* =========================
      🌐 HEADERS
   ========================= */
