f0cc50fb62c5deaf330dae20af98fb8ff204df02
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-08-30 16:46:12 +0530

    Comment out unused dataset endpoint code
    Comment out dataset endpoint code in CrossEvidenceBrain.js

--- api/CrossEvidenceBrain.js
+++ api/CrossEvidenceBrain.js
@@ -349,7 +349,7 @@ async function fetchLinkedInFromApify(url = "") {
                     source: null
                 };
             }
-
+/*
             const datasetEndpoint =
                 "https://api.apify.com/v2/actor-runs/" +
                 encodeURIComponent(runId) +
@@ -396,7 +396,7 @@ async function fetchLinkedInFromApify(url = "") {
             }
 
             var data = parsedDataset;
-
+*/
         } else {
 
             try {
